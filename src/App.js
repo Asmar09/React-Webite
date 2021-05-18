f9c80efbe2d5bc420fe57973f67438a5b4ff3720
Asmar <asmarmunir98@gmail.com>
2021-05-18 17:02:03 +0500

    Work Steps Flow complete

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Footer from './component/footer';
 import ContactUs from './AppData/ContactUs';
 import ExampleData from './AppData/ExampleData';
 import FeeStructure from './AppData/FeeStructure';
+import WorksSteps from './AppData/WorksSteps';
 
 function App() {
   return (
     <>
     <Header />
+    <WorksSteps />
     <FeeStructure />
     <ExampleData />
     <ContactUs />
