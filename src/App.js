39753256534a9a11c24cb34b7fc64ef45a52e552
Asmar <asmarmunir98@gmail.com>
2021-05-18 12:18:54 +0500

    Footer Set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import React from 'react';
 import Header from './component/header';
+import Footer from './component/footer';
 
 function App() {
   return (
     <>
     <Header />
+    <h1>Data Here</h1>
+    <h1>Data Here</h1>
+    <h1>Data Here</h1>
+    <h1>Data Here</h1>
+    <h1>Data Here</h1>
+
+    <Footer />
     </>
   );
 }
