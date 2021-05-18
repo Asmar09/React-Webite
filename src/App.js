0284da24719591a30466cfde9ea8f652696d1030
Asmar <asmarmunir98@gmail.com>
2021-05-18 13:24:02 +0500

    Deliverables Example Videos Set

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './component/header';
 import Footer from './component/footer';
 import ContactUs from './AppData/ContactUs';
+import ExampleData from './AppData/ExampleData';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
     <h1>Data Here</h1>
     <h1>Data Here</h1>
     <h1>Data Here</h1>
-    <h1>Data Here</h1>
+    <ExampleData />
     <ContactUs />
 
     <Footer />
---
+++ src/AppData/ExampleData.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import DiliverItems from './DataChunks/sliderData';
+
+const ExampleData = () => {
+    return (
+       <>
+
+<div className="mpartial_section howitworks css-13erfhv-Deliverables">
+    <div className="container">
+      <div className="title center">
+<div className="row"><div className="col"><h2 className="section_title_text">Example Deliverables</h2></div></div>
+      </div>
+      <h3 className="widget-title sub mt-1">Pre-Mitigation Scan + Post-Mitigation Scan = ESX &amp; SKX (TruePlan)</h3>
+      <div class="lazyload-wrapper">
+          <div className="deliver_widget_container">
+              <div className="lazyload-wrapper">
+                  <section><h3 className="widget-title"></h3>
+                  <div><iframe width="100%" height="480" src="https://my.matterport.com/show/?m=e5Wxtu8Arbx" frameborder="0" loading="lazy" allowfullscreen="" allow="vr"></iframe></div>
+                  </section>
+                  </div>
+                  <span className="numeric-character"></span>
+                  <div className="lazyload-wrapper">
+                      <section><h3 className="widget-title"></h3><div><iframe width="100%" height="480" src="https://my.matterport.com/show/?m=DhqzGgT7M1E" frameborder="0" loading="lazy" allowfullscreen="" allow="vr"></iframe></div>
+                      </section>
+                      </div>
+                      </div>
+                      </div>
+                      <span className="numeric-character"></span>
+     
+               {/* <DiliverItems /> */}
+ 
+                      
+
+
+    </div>
+</div>
+
+       </>
+    )
+}
+
+export default ExampleData
