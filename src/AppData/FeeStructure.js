98b353e621e0c40117dd5aefb2dd7d58ec102719
Asmar <asmarmunir98@gmail.com>
2021-05-19 15:28:05 +0500

    App Icon set

--- src/AppData/FeeStructure.js
+++ src/AppData/FeeStructure.js
@@ -1,8 +1,40 @@
 import React from "react";
 import Slider from '@material-ui/core/Slider';
-
+import { withStyles} from '@material-ui/core/styles';
 
 const FeeStructure = () => {
+
+  const PrettoSlider = withStyles({
+    root: {
+      color: '#52af77',
+      height: 8,
+    },
+    thumb: {
+      height: 24,
+      width: 24,
+      backgroundColor: '#fff',
+      border: '2px solid currentColor',
+      marginTop: -8,
+      marginLeft: -12,
+      '&:focus, &:hover, &$active': {
+        boxShadow: 'inherit',
+      },
+    },
+    active: {},
+    valueLabel: {
+      left: 'calc(-50% + 4px)',
+    },
+    track: {
+      height: 8,
+      borderRadius: 4,
+      color: '#0a5169'
+    },
+    rail: {
+      height: 10,
+      borderRadius: 4,
+    },
+  })(Slider);
+
     const marks = [
         {
           value: 0,
@@ -26,9 +58,6 @@ const FeeStructure = () => {
         },
       ];
       
-      function valuetext(value) {
-        return `${value}`;
-      }
   return (
     <div id="Fee-Structure">
       <div className="mpartial_section css-1jnmlvi-WaterFall">
@@ -40,14 +69,9 @@ const FeeStructure = () => {
               </div>
             </div>
           </div>
-          <Slider
-        defaultValue={25}
-        getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider-always"
-        step={10}
+       <PrettoSlider valueLabelDisplay="on" aria-label="pretto slider" defaultValue={20} 
         marks={marks}
-        valueLabelDisplay="on"
-      />
+        />
           <p className="section_title_description css-ulgnwk-WaterFall">
             Drag the slider around based on what you think it will cost to
             repair the property. <br />
@@ -55,7 +79,7 @@ const FeeStructure = () => {
           </p>
           <div className="partial_fee_btn">
             <i className="info_popup">i</i>
-            <label>7.22%</label>
+            <label>6.40%</label>
             <span>mpartial Fee</span>
           </div>
           <p className="waterfall_info_text">[$750 Minimum]</p>
